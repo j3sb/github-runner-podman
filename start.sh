7bd3d8e45ebca1408c62fb8baeee6dd2b7700a59
Jonas <jonas.bonnaudet@gmail.com>
2025-07-01 12:10:28 +0200

    basic runner

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#ORGANIZATION=$ORGANIZATION
+URL=$URL
+#ACCESS_TOKEN=$ACCESS_TOKEN
+
+#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$REG_TOKEN
+
+cd /home/docker/actions-runner
+
+./config.sh --unattended --url $URL --token ${REG_TOKEN}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
