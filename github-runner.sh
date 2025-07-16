91a3bb4c0b6b68d2de387e41fc07163ca39d460b
Jonas <jonas.bonnaudet@gmail.com>
2025-07-16 21:19:48 +0200

    only works with docker lol

--- github-runner.sh
+++ github-runner.sh
@@ -9,7 +9,7 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/dock/actions-runner
+cd /home/podude/actions-runner
 
 ./config.sh --unattended --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
@@ -22,6 +22,6 @@ trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
 
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_TOKEN}
+podman login -u ${DOCKER_USERNAME} -p ${DOCKER_TOKEN}
 
 ./run.sh & wait $!
